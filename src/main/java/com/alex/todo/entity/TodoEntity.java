e17b3e8a7e33ce769d74eee484c1002b48639c54
alex-pascual22 <alex.pascual1122@gmail.com>
2026-05-10 20:51:54 +0800

    Initial Commit

---
+++ src/main/java/com/alex/todo/entity/TodoEntity.java
@@ -0,0 +1,28 @@
+package com.alex.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TODO_ENTRY")
+@Getter
+@Setter
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="TODO_ID")
+    private Long id;
+
+    @Column(name = "TODO_TITLE", length = 20)
+    private String title;
+
+    @Column(name="TODO_DESCR", length = 50)
+    private String description;
+
+    @Column(name="TODO_STATUS", length = 20)
+    private String status;
+
+    @Column(name="TODO_DETAILS")
+    private String details;
+}
