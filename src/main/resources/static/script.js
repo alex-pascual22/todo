b62f11b364cac8baca1fc17acdda1ac1f7d61601
alex-pascual22 <alex.pascual1122@gmail.com>
2026-06-14 20:28:59 +0800

    added create to do ui and javascript

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,1 +1,24 @@
-const apiUrl = "http://localhost:8080/api/v1/todo";
+async function createTodo(){
+    // Read values from the page
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const details = document.getElementById("details").value;
+
+    // create to do object that matches CreateToDoDTO
+    const todo = {
+        title: title,
+        description: description,
+        details: details
+    }
+
+    // Send POST request to SpringBoot
+    const response = await fetch("/api/v1/todo/add", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(todo)});
+
+    // Check if response succeeded
+    if(response.ok){
+        alert("To Do Saved!");
+        window.location.href = "/index.html";
+    } else {
+        alert("Failed to create To Do");
+    }
+}
